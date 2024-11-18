a77a9fa9f92d77cadd120e1fbb9057aa8f5731d8
Jack <jack20220723@gmail.com>
2024-11-18 13:45:13 +0800

    Add fetching upstream logic

--- sync.py
+++ sync.py
@@ -17,16 +17,20 @@ def get_repo_name_from_url(url: str) -> str:
 if __name__ == "__main__":
 
     urls = [
-        "git@github.com:QubitPi/MiniSearch.git",
-        "git@github.com:QubitPi/docs.konghq.com.git",
-        "git@github.com:QubitPi/docker-kong.git",
-        "git@github.com:QubitPi/mlflow.git",
-        "git@github.com:QubitPi/elastic-docs.git",
-        "git@github.com:QubitPi/logstash-docs.git",
-        "git@github.com:QubitPi/logstash.git",
-        "git@github.com:QubitPi/docker-docs.git",
-        "git@github.com:QubitPi/docker-install.git",
+        ("git@github.com:QubitPi/MiniSearch.git", "git@github.com:felladrin/MiniSearch.git"),
+        ("git@github.com:QubitPi/docs.konghq.com.git", "git@github.com:Kong/docs.konghq.com.git"),
+        ("git@github.com:QubitPi/docker-kong.git", "git@github.com:Kong/docker-kong.git"),
+        ("git@github.com:QubitPi/mlflow.git", "git@github.com:mlflow/mlflow.git"),
+        ("git@github.com:QubitPi/elastic-docs.git", "git@github.com:elastic/docs.git"),
+        ("git@github.com:QubitPi/logstash-docs.git", "git@github.com:elastic/logstash-docs.git"),
+        ("git@github.com:QubitPi/logstash.git", "git@github.com:QubitPi/logstash.git"),
+        ("git@github.com:QubitPi/docker-docs.git", "git@github.com:docker/docs.git"),
+        ("git@github.com:QubitPi/docker-install.git", "git@github.com:docker/docker-install.git"),
     ]
 
     for repo_url in urls:
-        repo = Repo.clone_from(repo_url, "../daily-sync/{}".format(get_repo_name_from_url(repo_url)))
+        forked_repo = repo_url[0]
+        upstream_repo = repo_url[1]
+
+        repo = Repo.clone_from(forked_repo, "../daily-sync/{}".format(get_repo_name_from_url(forked_repo)))
+        repo.create_remote("upstream", upstream_repo) # https://gitpython.readthedocs.io/en/stable/tutorial.html#handling-remotes
